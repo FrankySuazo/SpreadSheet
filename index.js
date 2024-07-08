97e93598fc6781097ada0ee302d4145496685978
FrankySuazo <frankysuazo@outlook.com>
2024-07-08 14:02:17 -0400

    js

--- index.js
+++ index.js
@@ -92,11 +92,24 @@ const evalFormula = (x, cells) => {
 };
 
 window.onload = () => {
-    const container = document.getElementById("container");
-    const createLabel = (name) => {
-      const label = document.createElement("div");
-      label.className = "label";
-      label.textContent = name;
-      container.appendChild(label);
-    }
-  
+  const container = document.getElementById("container");
+  const createLabel = (name) => {
+    const label = document.createElement("div");
+    label.className = "label";
+    label.textContent = name;
+    container.appendChild(label);
+  };
+  const letters = charRange("A", "J");
+  letters.forEach(createLabel);
+  range(1, 99).forEach((number) => {
+    createLabel(number);
+    letters.forEach((letter) => {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = letter + number;
+      input.ariaLabel = letter + number;
+      input.onchange = update;
+      container.appendChild(input);
+    });
+  });
+};
